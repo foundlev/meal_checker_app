481bf95dcfa7b440e970c20a507d3e99c58b4884
foundlev <lev.lsb56@yandex.ru>
2024-10-04 01:03:03 +0200

    PWA

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const urlsToCache = [
+    '/icons/',
+    '/manifest.json',
+    'index.html',
+    'script.js',
+    'style.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        // Здесь вы можете вернуть запасной файл, если необходимо
+      })
+  );
+});
